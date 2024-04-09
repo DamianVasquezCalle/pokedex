2ba2b5d9a0d7f5e0ae9948bb50192ecadc1fda65
PedroDamianVasquezCalle <damian.vasquez@10pearls.com>
2024-04-08 21:55:46 -0400

    Delegate inmutability to npm-immutableJS

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import { Col, Spin } from "antd";
 import { getPokemonsWithDetails, setLoading } from "./actions";
 
 function App() {
-  const pokemons = useSelector((state) => state.pokemons);
-  const loading = useSelector((state) => state.loading);
+  const pokemons = useSelector((state) => state.get("pokemons")).toJS();
+  const loading = useSelector((state) => state.get("loading"));
 
   const distpach = useDispatch();
 
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,26 +1,36 @@
+import { fromJS } from "immutable";
 import { SET_POKEMONS, SET_LOADING, SET_FAVOURITE } from "../actions/types";
 
-const initialState = {
+const initialState = fromJS({
   pokemons: [],
   loading: false,
-};
+});
 
 export const pokemonsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_POKEMONS:
-      return { ...state, pokemons: action.payload };
+      return state.setIn(["pokemons"], fromJS(action.payload));
     case SET_LOADING:
-      return { ...state, loading: action.payload };
+      return state.setIn(["loading"], action.payload);
     case SET_FAVOURITE:
-      const newPokemonList = [...state.pokemons];
-      const currentPokemonIndex = newPokemonList.findIndex(
-        (x) => x.id === action.payload.id
-      );
-      if (currentPokemonIndex >= 0) {
-        newPokemonList[currentPokemonIndex].favorite =
-          !newPokemonList[currentPokemonIndex].favorite;
+      const currentPokemonIndex = state
+        .get("pokemons")
+        .findIndex((pokemon) => pokemon.get("id") === action.payload.id);
+
+      if (currentPokemonIndex < 0) {
+        return state;
       }
-      return { ...state, pokemons: newPokemonList };
+
+      const isFavourite = state.getIn([
+        "pokemons",
+        currentPokemonIndex,
+        "favorite",
+      ]);
+
+      return state.setIn(
+        ["pokemons", currentPokemonIndex, "favorite"],
+        !isFavourite
+      );
     default:
       return state;
   }
